2103184a9cfc6ed21fe781460c2695f1da5f724a
EvgeniyGorbenko1 <evgeniy.gorbenko2@gmail.com>
2025-09-29 19:13:13 +0300

    Builder

---
+++ src/main/java/Pattern/Builder/Main.java
@@ -0,0 +1,12 @@
+package Pattern.Builder;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car.Builder().setBrand("Audi").setYear(1996).setPower(150).setMaxSpeed(280).build();
+        Car car2 = new Car.Builder().setBrand("Ford").setYear(2004).setPower(100).setMaxSpeed(190).build();
+        System.out.println(car2.getPower());
+    }
+
+}
