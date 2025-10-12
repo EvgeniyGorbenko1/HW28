98932734e00356d854bb1be6542f5a4a1718b115
EvgeniyGorbenko1 <evgeniy.gorbenko2@gmail.com>
2025-10-12 22:48:02 +0300

    CRUD , READ ==null???

--- src/main/java/com/tms/CRUD/Main.java
+++ src/main/java/com/tms/CRUD/Main.java
@@ -5,19 +5,21 @@ import java.sql.SQLException;
 import java.time.LocalDate;
 
 public class Main {
-     public static void main(String[] args) throws SQLException, ClassNotFoundException, IOException, SQLException, IOException {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException, IOException, SQLException, IOException {
         DataBaseConnection db = new DataBaseConnection();
-      db.getConnection();
-      UserRepository ur = new UserRepository();
+        db.getConnection();
+        UserRepository ur = new UserRepository();
 
-    User user = new User(0, "Alex3", "Qwerty123", "Max", "Efremov", (LocalDate) null, null );
-    User user1 = new User(0, "Alex4", "Qwerty123", "Max", "Efremov", (LocalDate) null, null );
-   // ur.addUser(user);
-    //ur.addUser(user1);
-    System.out.println(ur.getAllUsers());
-    System.out.println();
-    ur.deleteUser(4);
+        User user = new User(0, "Alex3", "Qwerty123", "Max", "Efremov", (LocalDate) null, null);
+        User user1 = new User(0, "Alex4", "Qwerty123", "Max", "Efremov", (LocalDate) null, null);
+        // ur.addUser(user);
+        //ur.addUser(user1);
+        System.out.println(ur.getAllUsers());
+        //DELETE USER(choose id)
+        ur.deleteUser(4);
+        //UPDATE USER(insert id)
+        ur.updateUser("lexa", "po4emyYa", "Denis", "Mirniy", 1);
 
 
-
-}}
+    }
+}
--- src/main/java/com/tms/CRUD/UserRepository.java
+++ src/main/java/com/tms/CRUD/UserRepository.java
@@ -2,6 +2,7 @@ package com.tms.CRUD;
 
 import com.tms.pattern.decorator.model.Circle;
 
+import javax.xml.crypto.Data;
 import java.io.IOException;
 import java.sql.*;
 import java.time.LocalDate;
@@ -11,7 +12,7 @@ import java.util.List;
 
 public class UserRepository {
     private static final String INSERT_USER = "INSERT INTO users(id, username, password, first_name, last_name, created_at, updated_at) VALUES (DEFAULT,?,?,?,?,?,?)";
-
+    private static final String UPDATE_USER = "UPDATE users SET username = ?, password = ?, first_name = ?, last_name = ?, updated_at = CURRENT_TIMESTAMP WHERE id = ? ";
     public void addUser(User user) {
         try(Connection connection = DataBaseConnection.getConnection();
         PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USER)) {
@@ -29,24 +30,39 @@ public class UserRepository {
     }
 
     public List<User> getAllUsers() {
-        List<User> users = new ArrayList<User>();
+        List<User> users = new ArrayList();
         try (Connection connection = DataBaseConnection.getConnection();
              Statement stmt = connection.createStatement();
-             ResultSet rs = stmt.executeQuery("select * from users")) {
+             ResultSet rs = stmt.executeQuery("SELECT id, username, password, first_name, last_name, created_at, updated_at FROM users")) {
             while (rs.next()) {
-                int id = rs.getInt("id");
-                String username = rs.getString("username");
-                String password = rs.getString("password");
-                String first_name = rs.getString("first_name");
-                String last_name = rs.getString("last_name");
-                String created_at = String.valueOf(rs.getTimestamp("created_at"));
-                String updated_at = String.valueOf(rs.getTimestamp("updated_at"));
+                int id = rs.getInt(1);
+                String username = rs.getString(2);
+                String password = rs.getString(3);
+                String first_name = rs.getString(4);
+                String last_name = rs.getString(5);
+                String created_at = String.valueOf(rs.getTimestamp(6));
+                String updated_at = String.valueOf(rs.getTimestamp(7));
                 users.add(new User(id, username, password, first_name, last_name, created_at, updated_at));
             }
         } catch (SQLException | ClassNotFoundException | IOException e) {
             throw new RuntimeException(e);
         }
+
         return users;
+    }
+
+    public void updateUser( String newUsername, String newPassword, String newFirstName, String newLastName, int id) {
+       try(Connection connection = DataBaseConnection.getConnection();
+           PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_USER)){
+           preparedStatement.setString(1, newUsername);
+           preparedStatement.setString(2, newPassword);
+           preparedStatement.setString(3, newFirstName);
+           preparedStatement.setString(4, newLastName);
+           preparedStatement.setInt(5, id);
+           preparedStatement.executeUpdate();
+       } catch (SQLException | ClassNotFoundException | IOException e) {
+           throw new RuntimeException(e);
+       }
     }
 
     public void deleteUser(int id) {
